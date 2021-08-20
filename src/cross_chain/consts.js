52ddbf4006a084899d76b7bc0f8451104dd9e0fc
rupansh <dima@xp.network>
2021-08-20 19:59:40 +0530

    balance: divide by decimals before sending

--- src/cross_chain/consts.js
+++ src/cross_chain/consts.js
@@ -1,25 +1,29 @@
+import BigNumber from "bignumber.js";
 import { coins } from "../config";
 
 export const CHAIN_INFO = {
-    'XP.network': { nonce: 1, native: 'XPNET' },
-    'Elrond': { nonce: 2, native: 'EGLD' },
+    'XP.network': { nonce: 1, native: 'XPNET', decimals: 1E12 },
+    'Elrond': { nonce: 2, native: 'EGLD', decimals: 1E18 },
     'HECO': {
         nonce: 3,
         native: coins[2],
         chainId: 256,
-        rpcUrl: "https://http-testnet.hecochain.com"
+        rpcUrl: "https://http-testnet.hecochain.com",
+        decimals: 1E18
     },
     'BSC': {
         nonce: 4,
         native: coins[3],
         chainId: 97, 
-        rpcUrl: "https://data-seed-prebsc-1-s1.binance.org:8545"
+        rpcUrl: "https://data-seed-prebsc-1-s1.binance.org:8545",
+        decimals: 1E18
     },
     'Ropsten': {
         nonce: 5,
         native: coins[4],
         chainId: 3,
-        rpcUrl: "https://ropsten.infura.io/v3/182b3d3fb2d14d5fbe7421348624d1ce"
+        rpcUrl: "https://ropsten.infura.io/v3/182b3d3fb2d14d5fbe7421348624d1ce",
+        decimals: 1E18
     }
 };
 
